214e5dd6c9a1d6220ab5904b9b14bb38cbcdf870
Alexander Letnikow <reclamme@gmail.com>
2019-01-22 20:31:03 +0200

    Implement Token

---
+++ src/Token.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wearesho\Yii2\Authorization;
+
+/**
+ * Class Token
+ * @package Wearesho\Yii2\Authorization
+ */
+class Token
+{
+    /** @var string */
+    protected $access;
+
+    /** @var string */
+    protected $refresh;
+
+    public function __construct(string $access, string $refresh)
+    {
+        $this->access = $access;
+        $this->refresh = $refresh;
+    }
+
+    public function getAccess(): string
+    {
+        return $this->access;
+    }
+
+    public function getRefresh(): string
+    {
+        return $this->refresh;
+    }
+}
