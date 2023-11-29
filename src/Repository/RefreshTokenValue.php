6ad03d5055c3c1c1ccb389b59e5619a477c89e9e
Horat1us <reclamme@gmail.com>
2023-11-29 18:24:14 +0100

    Implement RefreshTokenValue to represent tuple of access token and user id so refresh token could be used to generate new access token even if access token is no longer available in storage

---
+++ src/Repository/RefreshTokenValue.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wearesho\Yii2\Authorization\Repository;
+
+class RefreshTokenValue
+{
+
+    private string $accessToken;
+    private int $userId;
+
+    public function __construct(string $accessToken, int $userId)
+    {
+        $this->accessToken = $accessToken;
+        $this->userId = $userId;
+    }
+
+    public function getAccessToken(): string
+    {
+        return $this->accessToken;
+    }
+
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+}
